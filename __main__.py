20f0b791fc1470ec142e36718bce7f5fc1940961
skalyzzz <akbyre200933z@mail.ru>
2025-05-13 21:18:55 +0300

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,3 @@
-"""Главный файл программы, запускает приложение и настраивает журналирование."""
-
 import logging.config
 import os
 
@@ -7,12 +5,10 @@ from app import create_app
 from modules.constants import LOGGING_CONFIG_FILE
 
 if __name__ == '__main__':
-    # Настройка журналирования
     logging.config.fileConfig(LOGGING_CONFIG_FILE)
 
     logging.info('Запуск приложения')
 
-    # Создание и запуск приложения
     app = create_app()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
